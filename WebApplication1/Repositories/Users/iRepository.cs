6f9eb8aec12fdedf79fd58179e3ea7b72e7f398d
Sebastian Chairez <sebax86@icloud.com>
2025-05-07 16:51:47 -0600

    Repositories/iRepository UPDATED

--- WebApplication1/Repositories/Users/iRepository.cs
+++ WebApplication1/Repositories/Users/iRepository.cs
@@ -1,6 +1,10 @@
 namespace WebApplication1.Repositories.Users;
 
-public class iRepository
+public interface IRepository<T>
 {
-    
+    public Task<T?> Create(T entity);
+    public Task<T?> GetById(Guid? id);
+    public Task<T?> Update(T entity);
+    public Task<IEnumerable<T>> GetAll(Guid userId);
+    public Task<bool> Delete(Guid id);
 }
